e8e90609b0e5bab67229a205c75cbb54993d72be
cloudnull <kevin@bkintegration.com>
2013-04-29 13:29:13 -0500

    sets up the default logging behavior for the library.

--- bookofnova/logger.py
+++ bookofnova/logger.py
@@ -15,7 +15,6 @@
 # ==============================================================================
 import logging
 import os
-import sys
 from bookofnova.info import __appname__ as appname
 
 
@@ -23,29 +22,41 @@ class NoLogLevelSet(Exception):
     pass
 
 
-def logger_setup(log_level, output=sys.stdout):
-    """
-    Setup logging for your application
-    """
-    logger = logging.getLogger("%s" % (appname.upper()))
-    if log_level.upper() == 'DEBUG':
-        logger.setLevel(logging.DEBUG)
-    elif log_level.upper() == 'INFO':
-        logger.setLevel(logging.INFO)
-    elif log_level.upper() == 'WARN':
-        logger.setLevel(logging.WARN)
-    elif log_level.upper() == 'ERROR':
-        logger.setLevel(logging.ERROR)
-    else:
-        raise NoLogLevelSet('I died because you did not set a known log level')
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s"
-                                  " - %(message)s")
+class Logging(object):
+    def __init__(self, log_level, log_file=None):
+        self.log_level = log_level
+        self.log_file = log_file
 
-    # Building Handeler
-    handler = logging.FileHandler(output)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    return logger
+    def logger_setup(self):
+        """
+        Setup logging for your application
+        """
+        logger = logging.getLogger("%s" % (appname.upper()))
+
+        avail_level = {'DEBUG': logging.DEBUG,
+                       'INFO': logging.INFO,
+                       'CRITICAL': logging.CRITICAL,
+                       'WARN': logging.WARN,
+                       'ERROR': logging.ERROR}
+        _log_level = self.log_level.upper()
+        if _log_level in avail_level:
+            lvl = avail_level[_log_level]
+            logger.setLevel(lvl)
+            formatter = logging.Formatter("%(asctime)s - %(name)s:%(levelname)s ==>"
+                                          " %(message)s")
+        else:
+            raise NoLogLevelSet('I died because you did not set a known log level')
+
+        # Building Handeler
+        if self.log_file:
+            handler = logging.FileHandler(self.log_file)
+        else:
+            handler = logging.StreamHandler()
+
+        handler.setLevel(lvl)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        return logger
 
 
 def return_logfile(filename):
@@ -73,3 +84,14 @@ def return_logfile(filename):
                 except Exception:
                     logfile = '%s' % logname
         return logfile
+
+
+def load_in(log_file=None, log_level='info', output=None):
+    if not output:
+        if log_file:
+            _log_file = return_logfile(filename=log_file)
+            log = Logging(log_level=log_level, log_file=_log_file)
+            output = log.logger_setup()
+        else:
+            output = Logging(log_level=log_level).logger_setup()
+    return output
